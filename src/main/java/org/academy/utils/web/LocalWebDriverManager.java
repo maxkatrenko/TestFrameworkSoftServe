18752635ecf14d8dfabb711b41d1fd7fb57738bd
maxkatrenko <bestintheworldqq@gmail.com>
2020-06-29 19:35:49 +0300

    minor changes

--- src/main/java/org/academy/utils/web/LocalWebDriverManager.java
+++ src/main/java/org/academy/utils/web/LocalWebDriverManager.java
@@ -85,7 +85,7 @@ public class LocalWebDriverManager {
                 RemoteWebDriver driver = null;
                 try {
                     driver = new RemoteWebDriver(
-                            URI.create("http://172.17.0.2:4444/wd/hub").toURL(),
+                            URI.create("http://localhost:4444/wd/hub").toURL(),
                             capabilities
                     );
                 } catch (MalformedURLException e) {
